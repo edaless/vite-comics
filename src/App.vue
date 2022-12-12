b98a363d4eefeea7b21544ffe9b56a7f8cd8b6ca
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2022-12-12 21:54:12 +0100

    un po' di macro e immagini in public

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,73 @@
+
+<!-- 
+Create un nuovo progetto utilizzando Vite e Vue 3 
+e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+Quando la struttura a macroblocchi è pronta, 
+popolate le voci di menu dinamicamente usando i data del componente.
+Per oggi diamo priorità alla struttura: quando è tutto bello solido, 
+passiamo al Sass (cioè la definizione dei dettagli del layout sempre tramite Sass)!
+Bonus:
+Creare un componente aggiuntivo per gestire la fascia azzurra con le icone.
+ -->
+
+
+
+
+
+
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="container">
+    <header>
+
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
 <style lang="scss" scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-
-
-  &:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto', sans-serif;
 }
 
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+
+#container {
+
+  width: 100vw;
+  height: 100vh;
+  background-color: yellow;
+  display: flex;
+  flex-direction: column;
+}
+
+header {
+  width: 100%;
+  height: 10vh;
+  background-color: white;
+}
+
+main {
+  width: 100%;
+  height: 100%;
+  background-color: rgb(7, 102, 255);
+}
+
+footer {
+  width: 100%;
+  height: 480px;
+  background-color: rgb(5, 230, 39);
 }
 </style>
